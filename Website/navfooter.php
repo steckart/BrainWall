c4142f61297828606b62df1594e7078f3321b067
Kevin Lehmann <58291919+klehmann55@users.noreply.github.com>
2020-01-06 11:12:39 +0100

    Update navfooter.php

--- Website/navfooter.php
+++ Website/navfooter.php
@@ -1,7 +1,15 @@
 <!-- FOOTER-NAVIGATION ========================================================================================= -->
 
 		<footer id="footernav">
-			<ul>			
+			<ul>
+				<li>
+					<a href="about.php" title="About" id="about"
+					   class="">Über</a>
+				</li>
+				<li>
+					<a href="terms.php" title="Terms" id="terms"
+					   class="">AGB & DSGVO</a>
+				</li>
 				<li>
 					<a href="legal.php" title="Legal" id="legal"
 					   class="">Impressum</a>
@@ -11,8 +19,8 @@
 					   class="">Kontakt</a>
 				</li>
 				<li>
-					<a href="terms.php" title="Terms" id="terms"
-					   class="">AGB & DSGVO</a>
+					<a href="jobs.php" title="Jobs" id="jobs"
+					   class="">Jobs</a>
 				</li>
 			</ul>
 		</footer>
